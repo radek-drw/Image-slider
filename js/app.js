55792f5d9fa669f8a8762cf38d7dcb3bc7162b48
radek-drw <rdrweski@gmail.com>
2021-10-05 18:10:51 +0100

    Added resizing function relative to the browser

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 import '../sass/style.scss';
+
+window.addEventListener('DOMContentLoaded', () => {
+   const imagesContainerEl = document.querySelector('.slider__images-container');
+   const img1El = document.querySelector('.slider__image-container--first img');
+   const img2El = document.querySelector('.slider__image-container--second img');
+
+   function adjustImagesSize() {
+
+      const imagesContainerWidth = imagesContainerEl.offsetWidth;
+      img1El.style.width = `${imagesContainerWidth}px`;
+      img2El.style.width = `${imagesContainerWidth}px`;
+   }
+
+   window.addEventListener('resize', adjustImagesSize);
+
+   adjustImagesSize();
+});
